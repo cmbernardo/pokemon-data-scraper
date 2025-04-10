d85d9969a88f9ae532be2cc65f5c5d3b317334b8
cmbernardo <chrismoirabernardo@gmail.com>
2025-04-11 00:48:07 +0800

    Add imports + initial code

--- pokemondb-scraper.py
+++ pokemondb-scraper.py
@@ -1,1 +1,11 @@
-print("Hello World")
+from bs4 import BeautifulSoup
+import requests
+
+pokedex_url = "https://pokemondb.net/pokedex/all"
+pokedex_result = requests.get(pokedex_url)
+
+pokedex_doc = BeautifulSoup(pokedex_result.text, "html.parser")
+
+pokemon_list = list(dict.fromkeys(pokedex_doc.find_all('a', class_="ent-name")))
+
+print(pokemon_list)
