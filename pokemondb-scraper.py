808e5d1432503ecda925aa5f1c0fed129c757d0a
cmbernardo <chrismoirabernardo@gmail.com>
2025-07-19 20:31:51 +0800

    Add image url and evolution list

--- pokemondb-scraper.py
+++ pokemondb-scraper.py
@@ -8,36 +8,55 @@ pokedex_soup = BeautifulSoup(pokedex_response.text, "html.parser")
 
 # list all pokemon names without duplicates
 pokemon_href = list(dict.fromkeys(pokedex_soup.find_all("a", class_ = "ent-name")))
-print(pokemon_href)
-
-pokemon_details = []
-pokemon_stats = []
 pokemon_data = []
 
+print(f"[FETCH DATA]: START")
+
 for href in pokemon_href:
+    data = {}
+    
     pokemon_url = "https://pokemondb.net" + href["href"]
     pokemon_response = requests.get(pokemon_url)
     pokemon_soup = BeautifulSoup(pokemon_response.text, "html.parser")
 
     # Pokemon Details
     pokemon_id = int(pokemon_soup.find("th", string = "National №").find_next("td").text)
+    pokemon_id = f"{pokemon_id:05d}"
+    data["Pokemon ID"] = pokemon_id
+    
     pokemon_name = pokemon_soup.find("h1").text
+    data["Name"] = pokemon_name
+    
     pokemon_type = [type.text for type in pokemon_soup.find("th", string = "Type").find_next("td").find_all("a")]
     pokemon_type1 = pokemon_type[0]
-    pokemon_type2 = ""
+    data["Type 1"] = pokemon_type1
     
+    pokemon_type2 = ""
     if (len(pokemon_type) == 2):
         pokemon_type2 = pokemon_type[1]
+        data["Type 2"] = pokemon_type2
         
-    pokemon_description = ""
-    
-    if (pokemon_soup.find("h2", string = "Pokédex entries")):
-        pokemon_description = pokemon_soup.find("h2", string = "Pokédex entries").find_next("div").find("td", class_ = "cell-med-text").text
+    pokemon_description = pokemon_soup.find("h2", string = "Pokédex entries")
+    if (pokemon_description):
+        pokemon_description = pokemon_description.find_next("div").find("td", class_ = "cell-med-text").text
+        data["Description"] = pokemon_description
         
     pokemon_abilities = ', '.join([ability.text for ability in pokemon_soup.find("th", string = "Abilities").find_next("td").find_all("a")])
+    data["Abilities"] = pokemon_abilities
+    
     pokemon_catch_rate = pokemon_soup.find("th", string = "Catch rate").find_next("td").text.strip().split()[0]
+    data["Catch Rate"] = pokemon_catch_rate
+    
     pokemon_gender = ', '.join([gender.text for gender in pokemon_soup.find("th", string = "Gender").find_next("td").find_all("span")])
+    data["Gender"] = pokemon_gender
+    
     pokemon_generation = pokemon_soup.find("abbr").text.strip().split()[-1]
+    data["Generation"] = pokemon_generation
+    
+    pokemon_evolution = pokemon_soup.find("div", class_ = "infocard-list-evo")
+    if (pokemon_evolution):
+        pokemon_evolution = ', '.join([evolution.text for evolution in pokemon_evolution.find_all("a", class_ = "ent-name")])
+        data["Evolution"] = pokemon_evolution
 
     # Pokemon Stats
     pokemon_hp = pokemon_soup.find("th", string = "HP").find_next("td").text
@@ -48,31 +67,21 @@ for href in pokemon_href:
     pokemon_speed = pokemon_soup.find("th", string = "Speed").find_next("td").text
     pokemon_stats_total = pokemon_soup.find("th", string = "Total").find_next("td").text
     
-    pokemon_data.append({
-        "Pokemon ID": pokemon_id, 
-        "Name": pokemon_name, 
-        "Type 1": pokemon_type1, 
-        "Type 2": pokemon_type2, 
-        "Description": pokemon_description, 
-        "Abilities": pokemon_abilities, 
-        "Catch Rate": pokemon_catch_rate, 
-        "Gender": pokemon_gender,
-        "Generation": pokemon_generation,
-        "HP": pokemon_hp,
-        "Attack": pokemon_attack,
-        "Defense": pokemon_defense,
-        "Sp. Atk": pokemon_sp_atk,
-        "SP. Def": pokemon_sp_def,
-        "Speed": pokemon_speed,
-        "Total": pokemon_stats_total
-    })
+    data["HP"] = pokemon_hp
+    data["Attack"] = pokemon_attack
+    data["Defense"] = pokemon_defense
+    data["Sp. Atk"] = pokemon_sp_atk
+    data["SP. Def"] = pokemon_sp_def
+    data["Speed"] = pokemon_speed
+    data["Total"] = pokemon_stats_total
+    
+    # Pokemon Image Source
+    pokemon_img = pokemon_soup.find("div", class_ = "grid-col span-md-6 span-lg-4 text-center").find_next("img")["src"]
+    data["Pokemon Image"] = pokemon_img
+    
+    pokemon_data.append(data)
 
-details_df = pd.DataFrame(pokemon_details)
-stats_df = pd.DataFrame(pokemon_stats)
-data_df = pd.DataFrame(pokemon_data)
-    
-details_df.to_csv("./pokemon-details.csv", header = True, index = False, encoding = "utf-8-sig")
-stats_df.to_csv("./pokemon-stats.csv", header = True, index = False, encoding = "utf-8-sig")
-data_df.to_csv("./pokemon-data.csv", header = True, index = False, encoding = "utf-8-sig")
-    
-print("Done")
+print("[FETCH DATA]: DONE")
+
+pd.DataFrame(pokemon_data).to_json("./pokemon-data.json", orient = "records")
+print("[SAVE DATA]: Data saved to \"pokemon-data.json\"")
